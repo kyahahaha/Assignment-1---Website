dc2b02434deb1d65b8bfc4edbda3e7f7f0fad6ac
Ellysha Adriana Puteri Mohamad Yusof <S10267273@connect.np.edu.sg>
2024-11-30 23:34:38 +0800

    Add files via upload

---
+++ asg 1/script.js
@@ -0,0 +1,9 @@
+/* js for live */
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('live');
+  
+    button.addEventListener('click', function () {
+      alert("'Stille Nacht, Rosen Nacht' ticket x1 purchase made");
+    });
+  });
+  
